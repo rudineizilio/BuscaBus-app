8d315fe71328df8c932f3267c8d9b8595586aa4d
Rudinei Zilio <rudi_zilio@hotmail.com>
2021-11-26 23:17:52 -0300

    Retorna codigo para funcionar funcao do motorista

--- lib/controllers/driver/driver_controller.dart
+++ lib/controllers/driver/driver_controller.dart
@@ -1,3 +1,191 @@
+// import 'dart:async';
+// import 'package:buscabus/models/driver.dart';
+// import 'package:buscabus/models/location_close.dart';
+// import 'package:buscabus/models/location_open.dart';
+// import 'package:buscabus/widgets/default_toast.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:mobx/mobx.dart';
+// import 'package:permission_handler/permission_handler.dart' as ph;
+
+// part 'driver_controller.g.dart';
+
+// class DriverController = _DriverController with _$DriverController;
+
+// abstract class _DriverController with Store {
+//   @observable
+//   String busSelected;
+
+//   @observable
+//   String lineSelected;
+
+//   @observable
+//   bool sharedLocation = false;
+
+//   @observable
+//   Position driverPosition;
+
+//   @observable
+//   bool serviceEnabled = false;
+
+//   @observable
+//   LocationPermission locationPermission;
+
+//   @observable
+//   Timer timer;
+
+//   @observable
+//   StreamSubscription<Position> positionStream;
+
+//   @observable
+//   String status = 'Aguardando GPS';
+
+//   @observable
+//   Position positionLocation;
+
+//   @observable
+//   CollectionReference company = FirebaseFirestore.instance.collection('company');
+
+//   @observable
+//   CollectionReference locationClose = FirebaseFirestore.instance.collection('location_close');
+
+//   @observable
+//   CollectionReference locationOpen = FirebaseFirestore.instance.collection('location_open');
+
+//   @observable
+//   List<Driver> drivers = [];
+
+//   @observable
+//   LocationOpen currentLocation;
+
+//   @observable
+//   String currentLocationId;
+
+//   @observable
+//   GeoPoint locationStart;
+
+//   @action
+
+
+//   @action
+//   Future<void> listenPosition() async {
+//     ph.PermissionStatus permission = await ph.Permission.location.request();
+
+//     if (permission.isDenied) {
+//       DefaultToast(
+//         message: 'Parece que você não permitiu o uso do GPS, abra as configurações do aplicativo e libere a permissão',
+//         toastType: DefaultToastType.warning,
+//       );
+//     } else {
+//       bool gpsIsEnabled = await Geolocator.isLocationServiceEnabled();
+
+//       if (!gpsIsEnabled)
+//         DefaultToast(
+//           message: 'Obtendo a localização',
+//           toastType: DefaultToastType.info,
+//         );        
+//         setStatus('Obtendo a localização');
+
+//       positionStream = Geolocator.getPositionStream().listen((Position position) async {
+//         if (positionLocation == null) {
+//           positionLocation = position;
+//           setStatus('Localização obtida');
+//         }
+//       });
+//     }
+//   }
+
+//   @action
+//   void setStatus(String value) {
+//     status = value;
+//   }
+
+//   @action
+//   void setBus(String value) {
+//     busSelected = value;
+//   }
+
+//   @action
+//   void setLine(String value) {
+//     lineSelected = value;
+//   }
+
+//   @action
+//   void setSharedLocation(bool value) {
+//     sharedLocation = value;
+//   }
+
+//   @action
+//   Future<void> openDriverLocation(LocationOpen location) async {
+//     currentLocation = location;
+
+//     await addDriverLocation(currentLocation);
+//     setTimerPeriodic();
+//   }
+
+//   @action
+//   Future<void> addDriverLocation(LocationOpen location) async {
+//     DocumentReference docRef = await locationOpen.add(location.toMap());
+
+//     currentLocationId = docRef.id;
+//   }
+
+//   @action
+//   Future<void> deleteDriverLocation(LocationClose location) async {
+//     await locationClose.add(location.toMap()).catchError((error) => print('Failed to add Location: $error'));
+
+//     return locationOpen
+//       .doc(currentLocationId)
+//       .delete()
+//       .catchError((error) => print('Failed to delete Driver: $error'));    
+//   }  
+
+//   @action
+//   void setDriverPosition(Position position) {
+//     driverPosition = position;
+//   }
+
+//   @action
+//   Future<void> getPosition() async {
+//     await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low, forceAndroidLocationManager: true)
+//       .then((Position position) {
+//         setDriverPosition(position);
+//       }).catchError((e) {
+//         print(e);
+//       });
+
+//     updateDriverLocation();
+//   }
+
+//   @action
+//   Future<void> updateDriverLocation() {
+//     print('Update call');
+//     return locationOpen
+//       .doc(currentLocationId)
+//       .update({
+//         'bus': currentLocation.bus,
+//         'driver': currentLocation.driver,
+//         'startDate': currentLocation.startDate,
+//         'line': currentLocation.line,
+//         'lastUpdate': currentLocation.lastUpdate,
+//         'location': GeoPoint(driverPosition.latitude, driverPosition.longitude),
+//       })
+//       .catchError((error) => print("Failed to update Location: $error | LOCATION DOC ID $currentLocationId"));
+//   }  
+
+//   @action
+//   void setTimerPeriodic() {
+//     timer = Timer.periodic(Duration(seconds: 2), (Timer t) => getPosition());
+//   }
+
+//   @action
+//   void cancelTimer() {
+//     timer.cancel();
+//   }
+
+//   @computed
+//   bool get sharedButtonEnabled => (busSelected != null && lineSelected != null) ? true : false;
+// }
 import 'dart:async';
 import 'package:buscabus/models/driver.dart';
 import 'package:buscabus/models/location_close.dart';
@@ -60,12 +248,6 @@ abstract class _DriverController with Store {
 
   @observable
   String currentLocationId;
-
-  @observable
-  GeoPoint locationStart;
-
-  @action
-
 
   @action
   Future<void> listenPosition() async {
@@ -159,7 +341,8 @@ abstract class _DriverController with Store {
 
   @action
   Future<void> updateDriverLocation() {
-    print('Update call');
+    print('DRIVER LATITUDEEEEEEEE: ${driverPosition.latitude}');
+    print('DRIVER LONGITUDEEEEEEE: ${driverPosition.longitude}');
     return locationOpen
       .doc(currentLocationId)
       .update({
@@ -170,6 +353,7 @@ abstract class _DriverController with Store {
         'lastUpdate': currentLocation.lastUpdate,
         'location': GeoPoint(driverPosition.latitude, driverPosition.longitude),
       })
+      .then((value) => print('Driver updated'))
       .catchError((error) => print("Failed to update Location: $error | LOCATION DOC ID $currentLocationId"));
   }  
 
--- lib/screens/driver/index.dart
+++ lib/screens/driver/index.dart
@@ -1,18 +1,373 @@
+// import 'package:buscabus/controllers/company/company_controller.dart';
+// import 'package:buscabus/controllers/driver/driver_controller.dart';
+// import 'package:buscabus/controllers/login/login_controller.dart';
+// import 'package:buscabus/models/location_close.dart';
+// import 'package:buscabus/models/location_open.dart';
+// import 'package:buscabus/widgets/default_appBar.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_mobx/flutter_mobx.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:provider/provider.dart';
+// import 'package:intl/intl.dart';
+
+// import '../../widgets/default_toast.dart';
+// import '../../widgets/default_toast.dart';
+
+// class DriverScreen extends StatefulWidget {
+//   final String driverName;
+
+//   const DriverScreen({
+//     @required this.driverName,
+//   });
+
+//   @override
+//   _DriverScreenState createState() => _DriverScreenState();
+// }
+
+// class _DriverScreenState extends State<DriverScreen> {
+//   LoginController _loginController;
+//   DriverController _driverController;
+//   CompanyController _companyController;
+
+//   @override
+//   void didChangeDependencies() {
+//     _loginController = Provider.of<LoginController>(context);
+//     _driverController = Provider.of<DriverController>(context);
+//     _companyController = Provider.of<CompanyController>(context);
+//     _driverController.listenPosition();
+//     _driverController.getPosition();
+//     _driverController.setBus(null);
+//     _driverController.setLine(null);
+
+//     super.didChangeDependencies();
+//   }
+
+//   @override
+//   void dispose() {
+//     _driverController.positionStream.cancel();
+
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+//       appBar: DefaultAppBar(
+//         title: Text('Motorista'),
+//         automaticallyImplyLeading: false,
+//         actions: [
+//           IconButton(
+//               tooltip: 'Sair',
+//               icon: FaIcon(
+//                 FontAwesomeIcons.signOutAlt,
+//                 size: 20,
+//               ),
+//               onPressed: () {
+//                 _driverController.setBus(null);
+//                 _driverController.setLine(null);
+//                 _loginController.setLoginType('driver');
+//                 _loginController.logout();
+
+//                 Navigator.pop(context);
+//               }),
+//         ],
+//       ),
+//       body: Flex(
+//         direction: Axis.vertical,
+//         children: [
+//           Column(
+//             children: [
+//               Container(
+//                 width: double.infinity,
+//                 color: Color.fromRGBO(242, 242, 242, 1),
+//                 child: _driverController.sharedLocation
+//                   ? Image.asset('lib/assets/images/bus_start.gif', height: 250)
+//                   : Image.asset('lib/assets/images/bus_end.png', height: 250),
+//               ),
+//               SizedBox(
+//                 height: 30,
+//               ),
+//               Padding(
+//                 padding:
+//                     const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+//                 child: Container(
+//                   padding: const EdgeInsets.symmetric(horizontal: 5),
+//                   decoration: BoxDecoration(
+//                     border: Border.all(
+//                       width: 1,
+//                       color: Colors.grey,
+//                     ),
+//                     borderRadius: BorderRadius.circular(5),
+//                   ),
+//                   child: FutureBuilder(
+//                     future: _companyController.company.doc('262gZboPV0OZfjQxzfko').get(),
+//                     builder: (context, snapshot) {
+//                       if (snapshot.hasError) {
+//                         print('Error: ${snapshot.error}');
+//                       }
+//                       if (snapshot.hasData) {
+//                         final data = snapshot.data['bus'];
+
+//                         return Observer(builder: (_) {
+//                           return DropdownButton<dynamic>(
+//                             style: TextStyle(
+//                               color: Colors.grey[700],
+//                               fontSize: 16,
+//                             ),
+//                             underline: SizedBox(),
+//                             hint: Text(
+//                               _driverController.busSelected == null
+//                                   ? 'Selecione um ônibus'
+//                                   : _driverController.busSelected,
+//                               style: TextStyle(
+//                                 color: Colors.grey[700],
+//                                 fontSize: 16,
+//                               ),
+//                             ),
+//                             items: data.map<DropdownMenuItem<dynamic>>((e) {
+//                               return DropdownMenuItem(
+//                                 value: e['licensePlate'],
+//                                 child: Text(e['licensePlate']),
+//                               );
+//                             }).toList(),
+//                             isExpanded: true,
+//                             iconSize: 30,
+//                             onChanged: (value) => _driverController.setBus(value),
+//                             dropdownColor: Colors.white,
+//                           );
+//                         });
+//                       }
+//                       return Center(
+//                         child: CircularProgressIndicator(),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//               ),              
+//               Padding(
+//                 padding:
+//                     const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+//                 child: Container(
+//                   padding: const EdgeInsets.symmetric(horizontal: 5),
+//                   decoration: BoxDecoration(
+//                     border: Border.all(
+//                       width: 1,
+//                       color: Colors.grey,
+//                     ),
+//                     borderRadius: BorderRadius.circular(5),
+//                   ),
+//                   child: FutureBuilder(
+//                     future: _companyController.company.doc('262gZboPV0OZfjQxzfko').get(),
+//                     builder: (context, snapshot) {
+//                       if (snapshot.hasError) {
+//                         print('Error: ${snapshot.error}');
+//                       }
+//                       if (snapshot.hasData) {
+//                         final data = snapshot.data['lines'];
+
+//                         return Observer(builder: (_) {
+//                           return DropdownButton<dynamic>(
+//                             style: TextStyle(
+//                               color: Colors.grey[700],
+//                               fontSize: 16,
+//                             ),
+//                             underline: SizedBox(),
+//                             hint: Text(
+//                               _driverController.lineSelected == null
+//                                   ? 'Selecione uma linha'
+//                                   : _driverController.lineSelected,
+//                               style: TextStyle(
+//                                 color: _driverController.busSelected != null
+//                                     ? Colors.grey[700]
+//                                     : Colors.grey,
+//                                   fontSize: 16,
+//                               ),
+//                             ),
+//                             items: data.map<DropdownMenuItem<dynamic>>((e) {
+//                               return DropdownMenuItem(
+//                                 value: e['title'],
+//                                 child: Text(e['title']),
+//                               );
+//                             }).toList(),
+//                             isExpanded: true,
+//                             iconSize: 30,
+//                             onChanged: _driverController.busSelected != null
+//                                 ? (value) {
+//                                     _driverController.setLine(value);
+//                                   }
+//                                 : null,
+//                             dropdownColor: Colors.white,
+//                           );
+//                         });
+//                       }
+//                       return Center(
+//                         child: CircularProgressIndicator(),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 30,
+//               ),
+//               Observer(builder: (_) {
+//                 return RaisedButton(
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(100),
+//                   ),
+//                   child: Container(
+//                     padding: const EdgeInsets.symmetric(vertical: 15),
+//                     width: 150,
+//                     child: Center(
+//                       child: Text(
+//                         _driverController.sharedLocation
+//                           ? 'Parar'
+//                           : 'Iniciar',
+//                         style: TextStyle(
+//                           fontSize: 18,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   color: Theme.of(context).accentColor,
+//                   disabledColor: Theme.of(context).accentColor.withAlpha(100),
+//                   textColor: Colors.white,
+//                   onPressed: _driverController.sharedButtonEnabled
+//                     ? !_driverController.sharedLocation 
+//                       ? () {
+//                           _driverController.setSharedLocation(true);
+
+//                           _driverController.openDriverLocation(
+//                             LocationOpen(
+//                               bus: _driverController.busSelected,
+//                               line: _driverController.lineSelected,
+//                               driver: widget.driverName,
+//                               startDate: DateTime.now(),
+//                               location: GeoPoint(_driverController.driverPosition.latitude, _driverController.driverPosition.longitude),
+//                               lastUpdate: DateTime.now(),
+//                             ),
+//                           );
+
+//                           setState(() {});
+
+//                           DefaultToast(
+//                             message: 'Compartilhamento de localização iniciado',
+//                             toastType: DefaultToastType.info,
+//                           ).show(context);
+//                         }
+//                       : () {
+//                           _driverController.setSharedLocation(false);
+
+//                           _driverController.deleteDriverLocation(
+//                             LocationClose(
+//                               bus: _driverController.currentLocation.bus,
+//                               line: _driverController.currentLocation.line,
+//                               driver: _driverController.currentLocation.driver,
+//                               startDate: _driverController.currentLocation.startDate,
+//                               lastUpdate: _driverController.currentLocation.lastUpdate,
+//                               endDate: DateTime.now(),
+//                             ),
+//                           );
+
+//                           _driverController.cancelTimer();
+
+//                           setState(() {_driverController.locationClose.get();});
+
+//                           DefaultToast(
+//                             message: 'Compartilhamento de localização finalizado',
+//                             toastType: DefaultToastType.info,
+//                           ).show(context);                          
+//                         }
+//                     : null,
+//                 );
+//               }),
+//               SizedBox(
+//                 height: 20,
+//               ),
+//             ],
+//           ),
+//           Expanded(
+//             child: FutureBuilder(
+//               future: _driverController.locationClose.get(),
+//               builder: (context, snapshot) {
+//                 if (snapshot.hasError) {
+//                   print('Error: ${snapshot.error}');
+//                 }
+//                 if (snapshot.hasData) {
+//                   QuerySnapshot snap = snapshot.data;
+//                   List<dynamic> snaps = [];
+
+//                   snap.docs.forEach((e) {
+//                     if (e.data().containsValue(widget.driverName)) {
+//                       snaps.add(e);
+//                     }
+//                   });
+
+//                   return ListView.separated(
+//                     separatorBuilder: (context, index) {
+//                       return Divider(
+//                         height: 5,
+//                         color: Theme.of(context).accentColor,
+//                         indent: 70,
+//                       );
+//                     },
+//                     itemCount: snaps.length,
+//                     itemBuilder: (context, index) {
+//                       final dynamic item = snaps[index];
+
+//                       return Padding(
+//                         padding: const EdgeInsets.only(top: 10, bottom: 10),
+//                         child: ListTile(
+//                           leading: CircleAvatar(
+//                             radius: 25,
+//                             backgroundColor: Theme.of(context).accentColor,
+//                             child: Center(
+//                               child: FaIcon(
+//                                 FontAwesomeIcons.busAlt,
+//                                 color: Colors.white,
+//                                 size: 18,
+//                               ),
+//                             ),
+//                           ),
+//                           title: Text(item.get('line')),
+//                           subtitle: Column(
+//                             crossAxisAlignment: CrossAxisAlignment.start,
+//                             children: [
+//                               Text('Início: ${DateFormat('dd/MM/yyyy - HH:mm').format(item.get('startDate').toDate()).toString()}'),
+//                               Text('Fim: ${DateFormat('dd/MM/yyyy - HH:mm').format(item.get('endDate').toDate()).toString()}'),
+//                             ],
+//                           ),
+//                         ),
+//                       );
+//                     },
+//                   );
+//                 }
+//                 return Center(
+//                   child: CircularProgressIndicator(),
+//                 );
+//               },   
+//             ),            
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 import 'package:buscabus/controllers/company/company_controller.dart';
 import 'package:buscabus/controllers/driver/driver_controller.dart';
 import 'package:buscabus/controllers/login/login_controller.dart';
 import 'package:buscabus/models/location_close.dart';
 import 'package:buscabus/models/location_open.dart';
 import 'package:buscabus/widgets/default_appBar.dart';
+import 'package:buscabus/widgets/default_toast.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
-
-import '../../widgets/default_toast.dart';
-import '../../widgets/default_toast.dart';
 
 class DriverScreen extends StatefulWidget {
   final String driverName;
@@ -36,7 +391,6 @@ class _DriverScreenState extends State<DriverScreen> {
     _driverController = Provider.of<DriverController>(context);
     _companyController = Provider.of<CompanyController>(context);
     _driverController.listenPosition();
-    _driverController.getPosition();
     _driverController.setBus(null);
     _driverController.setLine(null);
 
@@ -79,13 +433,6 @@ class _DriverScreenState extends State<DriverScreen> {
         children: [
           Column(
             children: [
-              Container(
-                width: double.infinity,
-                color: Color.fromRGBO(242, 242, 242, 1),
-                child: _driverController.sharedLocation
-                  ? Image.asset('lib/assets/images/bus_start.gif', height: 250)
-                  : Image.asset('lib/assets/images/bus_end.png', height: 250),
-              ),
               SizedBox(
                 height: 30,
               ),
@@ -245,17 +592,10 @@ class _DriverScreenState extends State<DriverScreen> {
                               line: _driverController.lineSelected,
                               driver: widget.driverName,
                               startDate: DateTime.now(),
-                              location: GeoPoint(_driverController.driverPosition.latitude, _driverController.driverPosition.longitude),
+                              location: GeoPoint(-26.2047639,-52.6897099),
                               lastUpdate: DateTime.now(),
                             ),
                           );
-
-                          setState(() {});
-
-                          DefaultToast(
-                            message: 'Compartilhamento de localização iniciado',
-                            toastType: DefaultToastType.info,
-                          ).show(context);
                         }
                       : () {
                           _driverController.setSharedLocation(false);
@@ -272,13 +612,6 @@ class _DriverScreenState extends State<DriverScreen> {
                           );
 
                           _driverController.cancelTimer();
-
-                          setState(() {_driverController.locationClose.get();});
-
-                          DefaultToast(
-                            message: 'Compartilhamento de localização finalizado',
-                            toastType: DefaultToastType.info,
-                          ).show(context);                          
                         }
                     : null,
                 );
